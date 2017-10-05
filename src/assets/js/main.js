81eec6b3090c8304fb2ed15c77520ab16813fe64
druloksana <drul.oksana@gmail.com>
2017-10-05 23:33:47 +0300

    nothing to commit

---
+++ src/assets/js/main.js
@@ -0,0 +1,9 @@
+// $(document).ready(function() {
+//   $("#owl-demo1").owlCarousel({
+//     items : 1,
+//     lazyLoad : true,
+//     autoPlay : true,
+//     itemsDesktop : 2,
+//   });
+//   console.log('OKSANA');
+// });
